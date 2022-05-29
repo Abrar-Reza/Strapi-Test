cef1ebf2dbe53a294aa7ac6d85a565f211c9f074
Abrar Reza <55370306+Abrar-Reza@users.noreply.github.com>
2022-05-29 18:54:25 -0400

    production config

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: "https://strapi-todo.azurewebsites.net",
+    admin: {
+      url: "https://happy-bay-086e8680f.1.azurestaticapps.net",
+      serveAdminPanel: false,
+    },
+});
