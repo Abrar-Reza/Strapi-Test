df80555d61ec02632cf09def7ff777701110f1ec
Abrar Reza <55370306+Abrar-Reza@users.noreply.github.com>
2022-05-30 18:54:04 -0400

    admin fix

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  // host: env('HOST', '0.0.0.0'),
+  // port: env.int('PORT', 1337),
   url: "https://strapi-todo.azurewebsites.net",
   admin: {
     url: "https://strapi-todo.azurewebsites.net",
